c011d574bad40af1baaa48bfeedf34f0732cc468
zhaoke <kerncink@gmail.com>
2018-10-31 18:00:01 +0800

    update

---
+++ build/config.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  dev: {
+    // Paths
+    assetsSubDirectory: './static',
+    assetsPublicPath: '/',
+  },
+
+  build: {
+    assetsSubDirectory: './static',
+    assetsPublicPath: '/',
+  }
+}
